76af5780b946d422e44ab1c9a7146975f79357c6
Jun2Free <c16618@naver.com>
2022-05-21 08:42:35 -0400

    modity sphere.cpp

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -5,24 +5,48 @@
 
 class Sphere {
 public:
-    Sphere(double radius): radius_(radius), volume_(pi_ * 4/3 * pow(radius_,3)) {
-        if (radius <= 0) throw std::invalid_argument("radius must be positive");
-    };
+    Sphere(double radius): radius_(radius) {};
+
     // Accessor
     double GetRadius() {return radius_;}
     double GetVolume() {return volume_;}
 
+    // Mutator
+    void SetRadius(double radius) {
+        radius_ = radius;
+        validate();
+    }
+    void SetVolume() {
+        volume_ = pi_ * 4/3 * pow(GetRadius(),3);
+    }
+
 private:
     double const pi_{3.141592};     // if the order is changed, it causes an error
-    double const radius_;
-    double const volume_;
-
+    double radius_;
+    double volume_;
+    void validate() { if (GetRadius() <= 0) throw std::invalid_argument("radius must be positive");}
 };
 
 int main() {
     Sphere sphere(5);
     assert(sphere.GetRadius() == 5);
+    sphere.SetVolume();
     std::cout << sphere.GetVolume();
-    assert(abs(sphere.GetVolume() - 523.6) < 1);
+
+    std::cout << "\n";
+
+    sphere.SetRadius(3);
+    assert(sphere.GetRadius() == 3);
+    sphere.SetVolume();
+    std::cout << sphere.GetVolume();
+
+    bool caught{false};
+    try {
+        sphere.SetRadius(-1);
+    } catch (...) {
+        caught = true;
+    }
+    assert(caught);
+
     return 0;
 }
